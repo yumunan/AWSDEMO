523de706902d1351c5a931378f9893347bd3e8c0
yumunan <apedweb#123>
2019-11-08 01:41:39 +0800

    initial

---
+++ virtual-bank-customer/src/main/java/com/virtualBank/customer/VirtualBankCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.virtualBank.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualBankCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualBankCustomerApplication.class, args);
+	}
+
+}
